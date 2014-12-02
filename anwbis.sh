c2776211fcecad10625caeb39ffb6101feb99e62
Javier Martín-Caro <javier.martincaro@beeva.com>
2014-12-02 18:20:38 +0100

    anwbis.sh first commit

---
+++ anwbis.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Set Path
+
+PYTHON="/usr/bin/python"
+
+# Auxiliary Functions
+
+function contains(){  
+    local n=$#
+    local value=${!n}
+    for ((i=1;i < $n;i++)) 
+    do
+	:
+        if [ "${!i}" == "${value}" ]; then
+            echo "y"
+            return 0
+        fi
+    done
+    echo "n"
+    return 1
+}
+
+# Main program
+
+
+if [[ $(contains "$@" "-t") == "y" || $(contains "$@" "--teleport") == "y" ]]; then
+
+$PYTHON anwbis.py $@ 
+
+    if [ $? -eq 0 ]; then
+        echo "hola"
+    else
+        exit 1 
+    fi
+else 
+
+$PYTHON anwbis.py $@
+
+    if [ $? -eq 0 ]; then
+        exit 0
+    else
+        exit 1
+    fi
+fi
+
+#if [ $? -eq 0 ]; then
+#   ssh -i $(cat salida)
+#   exit;
+#else
+#   cat salida
+#   rm salida
+#   exit;
+#fi
