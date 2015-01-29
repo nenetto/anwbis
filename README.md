531e846d3ed19d5e161ae727fa7c6ebb46d81b53
Javier Martín-Caro <javier.martincaro@beeva.com>
2015-01-29 10:39:27 +0100

    setup.py support

---
+++ README.md
@@ -0,0 +1,14 @@
+ANWBIS - Amazon Account Access
+==================================================
+
+Introduction
+------------
+
+> usage: anwbis.py [-h] [--version] --project PROJECT --env
+>                 {dev,pre,pro,sbx,val,corp} [--role {developer,admin}]
+>                 [--region {eu-west-1,us-east-1,us-west-1}]
+>                 [--browser {firefox,chrome,link,default}]
+>                 [--list {all,bastion}] [--profile PROFILE]
+>                 [--teleport TELEPORT] [--filter FILTER] [--goodbye]
+>                 [--verbose]
+> anwbis.py: error: argument --project/-p is required
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+try:
+        from setuptools import setup
+except ImportError:
+        from distutils.core import setup
+
+config = {
+        'description': 'AnWbiS Amazon Account Access',
+        'author': 'Luis Gonzalez and Javier Martin-Caro',
+        'url': '',
+        'download_url': '',
+        'author_email': ['luis.gonzalez@beeva.com', 'javier.martincaro@beeva.com'],
+        'version': '1.1.2',
+        'install_requires': ['colorama >= 0.3.2', 'argparse >=1.2.2', 'boto >= 2.24.0'],
+        'packages': ['anwbis'],
+        'scripts': [],
+        'name': 'anwbis'
+}
+setup(**config)
